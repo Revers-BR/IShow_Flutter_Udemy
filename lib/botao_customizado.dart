7b5d16f2cf7db2441fc21d5a2b81beda9ec8c7e9
Revers-BR <reversrivair@gmail.com>
2024-11-13 16:31:27 -0300

    Adicionado animacao simutanea

--- lib/botao_customizado.dart
+++ lib/botao_customizado.dart
@@ -2,28 +2,76 @@ import 'package:flutter/material.dart';
 
 class BotaoCustomizado extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
+  final AnimationController animationController;
+
+  final Animation<double> largura;
+  final Animation<double> altura;
+  final Animation<double> opacidade;
+  final Animation<double> radius;
+
+  BotaoCustomizado({super.key, required this.animationController}):
+    largura = Tween<double>(
+      begin: 0, end: 500
+    ).animate(CurvedAnimation(
+      parent: animationController, 
+      curve: const Interval(0.5, 1)
+    )),
+
+    altura = Tween<double>(
+      begin: 0, end: 50
+    ).animate(CurvedAnimation(
+      parent: animationController, 
+      curve: const Interval(0.5, .7)
+    )),
+
+    opacidade = Tween<double>(
+      begin: 0, end: 1
+    ).animate(CurvedAnimation(
+      parent: animationController, 
+      curve: const Interval(0.6, 0.8)
+    )),
+
+    radius = Tween<double>(
+      begin: 0, end: 20
+    ).animate(CurvedAnimation(
+      parent: animationController, 
+      curve: const Interval(0.6, 1)
+    ))
+  ;
+
+  Widget _builder(BuildContext context, Widget? child){
     return InkWell(
       onTap: () {
         
       },
       child: Container(
-        height: 50,
+        height: altura.value,
+        width: largura.value,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(radius.value),
           gradient: const LinearGradient(colors: [
             Color.fromRGBO(255, 100, 127, 1),
             Color.fromRGBO(255, 123, 145, 1)
           ])
         ),
-        child: const Center(
-          child: Text("Entrar", style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold
-          )),
+        child: Center(
+          child: FadeTransition(
+            opacity: opacidade,
+            child: const Text("Entrar", style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            )),
+          ) 
         ),
       )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animationController, 
+      builder: _builder
     );
   }
 }
--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,5 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:ishow_flutter_udemy/botao_customizado.dart';
 import 'package:ishow_flutter_udemy/input_customizado.dart';
 
@@ -11,76 +11,145 @@ class Login extends StatefulWidget {
   State<Login> createState() => _Login();
 }
 
-class _Login extends State<Login> {
+class _Login extends State<Login> with SingleTickerProviderStateMixin{
+
+  AnimationController? _controller;
+
+  Animation? _animationBlur;
+  Animation? _animationFade;
+  Animation? _animationSize;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this
+    );
+
+    _animationBlur = Tween<double>(
+      begin: 10, end: 0
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!, 
+        curve: Curves.ease
+      )
+    );
+
+    _animationFade = Tween<double>(
+      begin: 0, end: 1
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!, 
+        curve: Curves.easeInOutQuint
+      )
+    );
+
+    _animationSize = Tween<double>(
+      begin: 0, end: 500
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!, 
+        curve: Curves.decelerate
+      )
+    );
+
+    _controller!.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     
     return Scaffold(
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 400,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("imagens/fundo.png"),
-                    fit: BoxFit.fill
-                  )
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 10,
-                      child: Image.asset("imagens/detalhe1.png")
-                    ),
-                    Positioned(
-                      left: 50,
-                      child: Image.asset("imagens/detalhe2.png")
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AnimatedBuilder(
+              animation: _animationBlur!, 
+              builder: (context, child) {
+                return Container(
+                  height: 400,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("imagens/fundo.png"),
+                      fit: BoxFit.fill
                     )
-                  ],
-                ),
-              ),
-
-              Padding(
-                padding: EdgeInsets.only(left: 30, right: 30),
-                child: Column(children: [
-                  Container(
-                    padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey[200]!,
-                          blurRadius: 15,
-                          spreadRadius: 4
-                        )
-                      ]
-                    ),
-                    child: Column(children: [
-                      InputCustomizado(
-                        hintText: "Email",
-                      ),
-                      InputCustomizado(
-                        hintText: "Senha",
-                        obscureText: true,
-                        icon: const Icon(Icons.lock),
-                      ),
-                    ]),
                   ),
-                  SizedBox(height: 20),
-                  BotaoCustomizado(),
-                  SizedBox(height: 20),
-                  Text("Esqueci minha senha!", style: TextStyle(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: _animationBlur!.value,
+                      sigmaY: _animationBlur!.value
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 10,
+                          child: FadeTransition(
+                            opacity: _animationFade as Animation<double>,
+                            child: Image.asset("imagens/detalhe1.png"),
+                          )
+                        ),
+                        Positioned(
+                          left: 50,
+                          child: FadeTransition(
+                            opacity: _animationFade as Animation<double>,
+                            child: Image.asset("imagens/detalhe2.png"),
+                          )
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+      
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30),
+              child: Column(children: [
+                AnimatedBuilder(
+                  animation: _animationSize!, 
+                  builder: (context, child) {
+                    return Container(
+                      width: _animationSize!.value,
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey[200]!,
+                            blurRadius: 15,
+                            spreadRadius: 4
+                          )
+                        ]
+                      ),
+                      child: const Column(children: [
+                        InputCustomizado(
+                          hintText: "Email",
+                        ),
+                        InputCustomizado(
+                          hintText: "Senha",
+                          obscureText: true,
+                          icon: Icon(Icons.lock),
+                        ),
+                      ]),
+                    );
+                  },
+                ),
+                const SizedBox(height: 20),
+                BotaoCustomizado(animationController: _controller!,),
+                const SizedBox(height: 20),
+                FadeTransition(
+                  opacity: _animationFade as Animation<double>,
+                  child: const Text("Esqueci minha senha!", style: TextStyle(
                     color: Color.fromRGBO(255, 100, 127, 1),
                     fontWeight: FontWeight.bold
-                  ))
-                ],),
-              ),
-            ],
-          ),
+                  )),
+                )
+              ],),
+            ),
+          ],
         ),
       ),
     );
